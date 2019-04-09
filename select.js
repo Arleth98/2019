1a1604301d2f4f90e185fa83c9c01774a876b78a
Arleth98 <syahrul.ramadan099@gmail.com>
2019-04-10 04:23:33 +0700

    Add files via upload

---
+++ select.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+function usagislct(rawData) {
+  var file = 0;
+  for (; 999 > file; file++) {
+    var cssresource;
+    var data = rawData["feed"]["entry"][file];
+    var value = data["title"]["$t"];
+    if (file == rawData["feed"]["entry"]["length"]) {
+      break;
+    }
+    var i = 0;
+    for (; i < data["link"]["length"]; i++) {
+      if ("alternate" == data["link"][i]["rel"]) {
+        cssresource = data["link"][i]["href"];
+        break;
+      }
+    }
+    document["write"]('<option value="' + cssresource + '">' + value + "</option>");
+  }
+};
